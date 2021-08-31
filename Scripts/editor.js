ffda5bbe0e4c1bc551a54bd2f777cf6195ead0ba
StevenChang21 <82698910+StevenChang21@users.noreply.github.com>
2021-08-31 15:23:46 +0800

    Replaced save color UI with DOM

--- Scripts/editor.js
+++ Scripts/editor.js
@@ -1,52 +1,53 @@
-function runEditor() {
-	const buttons = [];
+function globalSetAttributes(element, attributes) {
+	for (const key in attributes) {
+		element.setAttribute(key, attributes[key]);
+	}
+}
 
-	const editorButtons = Array.from(document.querySelectorAll(".editor-button"));
-	editorButtons.forEach((button) => {
+function runEditor() {
+	const initialButtons = document.querySelectorAll(".show-onReady");
+	initialButtons.forEach((button) => {
 		button.style.display = "inline";
 	});
+
 	const setColorsButton = document.querySelector("#customise-color-button");
+	const closeSetColorsButton = document.querySelector("#customise-color-close");
+
 	const useSavedColorButton = document.querySelector("#use-save-color-button");
-	const saveColorsButton = createButton("Save colors");
+	const saveColorsButton = document.querySelector("#save-color-button");
+	const editorContainer = document.querySelector("#editor-container");
+	const colorCustomisationContainer = document.querySelector("#color-customisation-container");
 
-	buttons.push(setColorsButton, saveColorsButton, useSavedColorButton);
-
-	const spacing = 30;
-	for (let i = 0, x = width / 2; i < buttons.length; i++) {
-		buttons[i].position(x + i * spacing, height);
-		x += buttons[i].size().width;
-	}
-
-	saveColorsButton.hide();
-
-	setColorsButton.mousePressed(() => {
-		buttonPos = setColorsButton.position();
-		setColorsButton.hide();
-		saveColorsButton.show();
-		const colorsData = config.assets.getChildAssetByType("Color").data;
-		const colorEditors = showColors(colorsData, buttonPos, undefined, undefined, { x: 0, y: 20 });
-		createCloseButton(
-			{
-				x: buttonPos.x,
-				y: buttonPos.y,
-			},
-			() => {
-				colorEditors.colorPickers.forEach((element) => element.colorPicker.remove());
-				colorEditors.colorLabels.forEach((element) => element.remove());
-				setColorsButton.show();
-				saveColorsButton.hide();
-			}
-		);
-
-		saveColorsButton.mousePressed(() => {
-			const data = {};
-
-			colorEditors.colorPickers.forEach((element) => {
-				data[element.key] = element.colorPicker.color();
-			});
-
-			createStringDict(data).saveJSON("m_Colors");
+	setColorsButton.addEventListener("click", () => {
+		editorContainer.children.forEach((child) => {
+			child.style.display = "none";
 		});
+		saveColorsButton.style.display = "inline";
+		closeSetColorsButton.style.display = "inline";
+		editorContainer.style.justifyContent = "space-between";
+		colorCustomisationContainer.style.display = "grid";
+		const colorAssets = config.assets.getChildAssetByType("Color").data;
+		showColors(colorAssets, colorCustomisationContainer);
+	});
+
+	closeSetColorsButton.addEventListener("click", () => {
+		initialButtons.forEach((child) => {
+			child.style.display = "inline";
+		});
+		saveColorsButton.style.display = "none";
+		closeSetColorsButton.style.display = "none";
+		document.querySelectorAll("input").forEach((input) => input.remove());
+		document.querySelectorAll("p").forEach((p) => p.remove());
+	});
+
+	saveColorsButton.addEventListener("click", (e) => {
+		const data = {};
+		const colorPickers = document.querySelectorAll("input");
+		const colorLabel = document.querySelectorAll("p");
+		for (let i = 0; i < colorPickers.length; i++) {
+			data[colorLabel[i].innerHTML] = hexToRgb(colorPickers[i].value);
+		}
+		createStringDict(data).saveJSON("m_Colors");
 	});
 
 	useSavedColorButton.mousePressed(() => {
@@ -78,22 +79,33 @@ function runEditor() {
 	});
 }
 
-function showColors(colorData, labelPosition, widthSpacing = 100, lineSpacing = 5, initialOffset = { x: 0, y: 0 }) {
+function hexToRgb(hex) {
+	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	return result
+		? {
+				r: parseInt(result[1], 16),
+				g: parseInt(result[2], 16),
+				b: parseInt(result[3], 16),
+		  }
+		: null;
+}
+
+function showColors(colorData, parent) {
 	const colorPickers = [];
 	const colorLabels = [];
 
-	let row = 0;
 	for (const key in colorData) {
 		const colorValues = colorData[key].levels;
-		const colorPicker = createColorPicker(color(colorValues[0], colorValues[1], colorValues[2]));
-		const colorLabel = createP(key);
-		colorLabel.style("color", color(255));
-		colorLabel.position(initialOffset?.x + labelPosition.x, initialOffset?.y + labelPosition.y + (lineSpacing + colorPicker.size().height) * row);
-		colorPicker.position(colorLabel.position().x + widthSpacing, colorLabel.position().y);
+		const colorLabel = createP(key).parent(parent);
+		const colorPicker = createColorPicker(color(colorValues[0], colorValues[1], colorValues[2])).parent(parent);
 		colorLabels.push(colorLabel);
 		colorPickers.push({ key, colorPicker });
-		row++;
 	}
+	let value = "";
+	for (let i = 0; i < colorLabels.length * 2 + 1; i += 2) {
+		value += "auto ";
+	}
+	parent.style.gridTemplateColumns = value;
 	return { colorPickers, colorLabels };
 }
 
